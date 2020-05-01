242e8f34f4020835fd344f38a2901a3f57dee18e
Sreelakshmi K B <sreelakshmikb@gmail.com>
2020-05-01 11:41:54 +0530

    cards and btns added

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-$(".top-btn").hover(
-    function () {
-      $(this).addClass("hvr-grow-shadow");
-    },
-    function () {
-      $(this).removeClass("hvr-grow-shadow");
-    }
-  );
+// $(".top-btn").hover(
+//     function () {
+//       $(this).addClass("hvr-grow-shadow");
+//     },
+//     function () {
+//       $(this).removeClass("hvr-grow-shadow");
+//     }
+//   );
